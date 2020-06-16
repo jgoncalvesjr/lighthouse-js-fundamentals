f9d8941355804d6c3dd463066a98049be9e67bab
Joao Goncalves Jr <jgoncalvesjr@gmail.com>
2020-06-16 21:37:48 +0000

    Udacity Intro to Javascript Lesson 6 (Arrays) Quizzes

---
+++ udacity/udacity-intro-to-javascript/quiz6-3.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `prices`
+ * - The variable `prices` should be an array having different values for the 1st, 3rd, and 7th elements
+ * - Your code should print `prices` to the console as an array. Do not iterate over the elements. 
+ */
+ 
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+
+prices[0] = 0.99;
+prices[2] = 69.95;
+prices[6] = 3.33;
+console.log(prices);
