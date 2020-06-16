3b32f3b71393be090ba68f4493e25a99ab70e383
Joao Goncalves Jr <jgoncalvesjr@gmail.com>
2020-06-16 16:13:21 +0000

    Lighthouse Compass <Declaring and Calling Functions> Assignment - part 2

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
+// Functions - part 1
+
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
@@ -5,3 +7,16 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// Functions - part 2 (Return x console.log)
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); // returns to console, but does NOT store info
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John'); // stores info, but does NOT return to console unless called
+console.log(greeting); 
