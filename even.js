e66213f1ab741d7ded5be9cba6751cd53b1c9007
Joao Goncalves Jr <jgoncalvesjr@gmail.com>
2020-06-16 17:12:54 +0000

    Lighthouse Compass <Declaring and Calling Functions> Assignment - part 3

---
+++ even.js
@@ -0,0 +1,6 @@
+const isEven  = function (num) {
+  return num % 2 === 0; // Stores Function return
+}
+
+console.log(isEven(10)); // 10 is even, returns True
+console.log(isEven(11)); // 11 is false, returns False
