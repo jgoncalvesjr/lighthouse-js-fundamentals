798d328e93e1e2a722efcfaf92c8b1f07a65728f
Joao Goncalves Jr <jgoncalvesjr@gmail.com>
2020-06-16 21:36:47 +0000

    Lighthouse Compass Programming Intro Arrays Exercise

---
+++ iceCream.js
@@ -0,0 +1,22 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+// 1. Add a flavour
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// 2. Get the First Flavour
+
+console.log(iceCreamFlavours[0]);
+
+// 3. Get the Last Flavour
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// 4. Total Number of Flavours
+
+console.log(iceCreamFlavours.length);
