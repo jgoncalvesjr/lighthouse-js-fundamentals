1a4e8de5494836fc29bb3e5c3f8de8ea7485854f
Joao Goncalves Jr <jgoncalvesjr@gmail.com>
2020-06-17 12:57:58 +0000

    Lighthouse Labs Compass exercise <Iterating Over Arrays> - Part 2

--- packing-list.js
+++ packing-list.js
@@ -4,3 +4,12 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// Just a demonstration that it's possible to iterate through an Array using while. For should be the standard, though
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
