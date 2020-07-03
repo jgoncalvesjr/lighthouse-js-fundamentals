2f6e19ac6ad59b294cb037cb5ee475dd9447a200
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-03 19:17:57 +0000

    Compass Prep Course Katas - 07/02/2020 update

---
+++ katas/kata13.js
@@ -0,0 +1,5 @@
+/*
+In this exercise, we will be building an advanced case maker that can convert strings into all different kinds of case styles; like camel, pascal, snake, or even kebab
+*/
+
+// This exercise was stored in a Secret Gist, as requested. Git placement for progress reference only.
